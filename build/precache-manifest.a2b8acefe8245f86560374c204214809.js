4414189befd801c46a4482beda283efbd5735f03
York <yorkshp@gmail.com>
2020-02-13 18:22:56 +0800

    new mod

---
+++ build/precache-manifest.a2b8acefe8245f86560374c204214809.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6d71b85fd68d8ab04398bfc9d573995",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97ba5b3f65cf0db921f7",
+    "url": "/static/css/2.daba7128.chunk.css"
+  },
+  {
+    "revision": "32c2425061f9ed834bda",
+    "url": "/static/css/main.a91124f9.chunk.css"
+  },
+  {
+    "revision": "97ba5b3f65cf0db921f7",
+    "url": "/static/js/2.4947fdfd.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.4947fdfd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "32c2425061f9ed834bda",
+    "url": "/static/js/main.d30a3084.chunk.js"
+  },
+  {
+    "revision": "e738b54ffe3b15816cf3",
+    "url": "/static/js/runtime-main.63c1efab.js"
+  }
+]);
